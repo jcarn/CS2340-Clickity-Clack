7a46f8ff49cf382f2e23d99e084ca0012fee25c4
HenrySaba <henry-saba@hotmail.com>
2017-02-23 17:51:19 -0500

    made lots of change

--- Watro/app/src/main/java/com/watro/clickityclack/watro/SuperUser.java
+++ Watro/app/src/main/java/com/watro/clickityclack/watro/SuperUser.java
@@ -1,10 +1,12 @@
+package com.watro.clickityclack.watro;
+
 abstract class SuperUser {
     
     protected String email;
     protected String id;
 
-    public SuperUser(String e, String id) {
-        this.email = e;
+    public SuperUser(String email, String id) {
+        this.email = email;
         this.id = id;
     }
 
@@ -12,7 +14,7 @@ abstract class SuperUser {
         return email;
     }
 
-    public void setEmail(String e) {
-        this.email = e;
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
