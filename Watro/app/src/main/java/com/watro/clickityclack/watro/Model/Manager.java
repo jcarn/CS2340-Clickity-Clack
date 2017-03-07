337c84391180c69a5de07e25c446cf4af47ab034
jcarn <jcarnahan3@gatech.edu>
2017-03-07 13:49:51 -0500

    javadoc'd stuff

--- Watro/app/src/main/java/com/watro/clickityclack/watro/Model/Manager.java
+++ Watro/app/src/main/java/com/watro/clickityclack/watro/Model/Manager.java
@@ -2,6 +2,15 @@ package com.watro.clickityclack.watro.Model;
 
 public class Manager extends Worker {
 
+    /**
+     * Constructor for Manager class
+     * @param firstName first name of user
+     * @param lastName surname of user
+     * @param email email of user
+     * @param id firebase generated user ID
+     * @param homeAddress home address of user
+     * @param userType user's permissions in the app, i.e. worker, manager, admin, etc.
+     */
     public Manager(String firstName, String lastName, String email, String id, String homeAddress, String userType) {
         super(firstName, lastName, email, id, homeAddress, userType);
     }
--- Watro/app/src/main/java/com/watro/clickityclack/watro/Model/SuperUser.java
+++ Watro/app/src/main/java/com/watro/clickityclack/watro/Model/SuperUser.java
@@ -5,15 +5,28 @@ public abstract class SuperUser {
     protected String email;
     protected String id;
 
+    /**
+     * Constructor for general user class from which all others inherit
+     * @param email email of user in form: name@host.ext
+     * @param id firebase generated ID of user
+     */
     public SuperUser(String email, String id) {
         this.email = email;
         this.id = id;
     }
 
+    /**
+     * Getter of user's email
+     * @return String email of user
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Setter of email
+     * @param email new email of user
+     */
     public void setEmail(String email) {
         this.email = email;
     }
