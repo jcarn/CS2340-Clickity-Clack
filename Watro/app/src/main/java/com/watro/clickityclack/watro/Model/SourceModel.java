24e5ab23d58a78b66956c5fa04f09d01734dd30c
John Carnahan <JohnGMCarnahan@gmail.com>
2017-04-06 18:07:06 -0400

    added junit test for sourcemodel

--- Watro/app/src/main/java/com/watro/clickityclack/watro/Model/SourceModel.java
+++ Watro/app/src/main/java/com/watro/clickityclack/watro/Model/SourceModel.java
@@ -14,7 +14,7 @@ public class SourceModel {
     private String waterCondition;
 
     public SourceModel(String date, String reportId, String reporterName, String location, String waterType, String waterCondition) {
-        this.date = date;
+        setDate(date);
         this.reportId = reportId;
         this.reporterName = reporterName;
         this.location = location;
@@ -35,7 +35,11 @@ public class SourceModel {
      * @param date date report was submitted
      */
     public void setDate(String date) {
-        this.date = date;
+        if(reportDate.split("-").length == 3){
+            this.date = date;
+        } else {
+            throw new IllegalArgumentException("Date was not in valid format");
+        }
     }
 
     /**
