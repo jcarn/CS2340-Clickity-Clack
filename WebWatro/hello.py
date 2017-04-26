0ea82c373f799cee0dfa8f06eb007c76ad066d9e
Amy Dao <amy.dao@gatech.edu>
2017-04-26 15:52:46 -0400

    progress on webapp

--- WebWatro/hello.py
+++ WebWatro/hello.py
@@ -4,8 +4,9 @@ from flask_googlemaps import Map
 import firebase_handler
 from model import user
 
-# db = firebase_handler.Database("https://watro-c0aaf.firebaseio.com/")
+user = ""
 
+db = firebase_handler.Database()
 app = Flask(__name__)
 
 app.config['GOOGLEMAPS_KEY'] = "AIzaSyCPOAyJ7sYZgkBaeHKdkzJirKox8QLJs0U"
@@ -16,16 +17,25 @@ def mainThing():
     # return "Hello World!"
     return render_template("home.html")
 
-@app.route("/map")
+@app.route("/main")
 # def mapHome():
 #     return render_template("mapHome.html")
-def mapview():
+def main():
 # creating a map in the view
+    user = str(db.users[0].firstName)
+    reports = db.reports
+    markerList = []
+    
+    for report in reports:
+        markerList.append(mapMarkers(report))
+
     mymap = Map(
-    identifier="view-side",
-    lat=37.4419,
-    lng=-122.1419,
-    markers=[(37.4419, -122.1419)]
+    identifier="map",
+    lat=26,
+    lng=-37,
+    zoom = 2,
+    style="height:500px;width:500px",
+    markers=markerList
     )
     sndmap = Map(
     identifier="sndmap",
@@ -33,20 +43,32 @@ def mapview():
     lng=-122.1419,
     markers=[
     {
-    'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-    'lat': 37.4419,
-    'lng': -122.1419,
-    'infobox': "<b>Hello World</b>"
+    'icon': '/static/images/minimarker.png',
+    'lat': reports[0].latitude,
+    'lng': reports[0].longitude,
+    'infobox': "Hi i'm amy"
     },
     {
-    'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+    'icon': '/static/images/minimarker.png',
     'lat': 37.4300,
     'lng': -122.1400,
     'infobox': "<b>Hello World from other place</b>"
     }
     ]
     )
-    return render_template('mapHome.html', mymap=mymap, sndmap=sndmap)
+    return render_template('mapHome.html', mymap=mymap, sndmap=sndmap, user=user)
+
+#makes dictionary with marker attributes from a single report - to be added to marker list
+def mapMarkers(report):
+    locationDict = {}
+    info = str(report.waterType) + ", " + str(report.waterCondition)
+    locationDict['icon'] = '/static/images/minimarker.png'
+    locationDict['lat'] = report.latitude
+    locationDict['lng'] = report.longitude
+    locationDict['infobox'] = info
+
+    return locationDict
+
 
 @app.route("/info")
 def info():
@@ -55,6 +77,23 @@ def info():
     print("Hi I am Amy " + user)
     return render_template('info.html', user=user, amy=amy)
 
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    global user
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db.login(username, password)
+
+        user = db.current_user().firstName
+
+        return redirect(url_for('main'))
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run()
